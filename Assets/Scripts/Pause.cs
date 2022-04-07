149d690ff6531deed419c00fa5115c65c02f8ec8
realforunreal <77521654+realforunreal@users.noreply.github.com>
2022-04-08 01:00:36 +0900

    AddPause

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+
+    public GameObject pausePanel;
+    Image myimg;
+
+    void Start()
+    {
+        myimg = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GamePause();
+        }
+    }
+    public void GamePause()
+    {
+        Time.timeScale = 0;
+        if(pausePanel != null && !pausePanel)
+        {
+            pausePanel.SetActive(true);
+            myimg.enabled = false;
+        }
+        else if (pausePanel)
+        {
+            pausePanel.SetActive(false);
+            Invoke("ResetTimeScale", 3);
+            myimg.enabled = true;
+        }
+        else
+        {
+            Debug.Log("pausePanel can't find");
+        }
+    }
+    void ResetTimeScale()
+    {
+        Time.timeScale = 1;
+    }
+}
